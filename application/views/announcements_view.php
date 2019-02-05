cda75881f845fb7b6c56b716c9c1bb1d1cbe659b
Angela Santos <2013055998@ust-ics.mygbiz.com>
2019-02-05 19:58:47 +0800

    fixed sidebar and put delete/edit on announcements

--- application/views/announcements_view.php
+++ application/views/announcements_view.php
@@ -85,7 +85,7 @@ $admin_fname = $this->session->userdata['login_success']['info']['admin_fname'];
                                                         <div class="ml-xl-auto ml-lg-auto ml-md-auto">
                                                             <button title="Delete announcement" type="button" class="btn btn-primary" id="ann" data-toggle="modal" data-target="#Delete" style="margin-right:3px;border-radius:90px 90px 90px 90px;padding:0px 8px;">
                                                                 <i class="icon ion-trash-a" style="font-size:15px;"></i></button>
-                                                            <button title="Edit announcement" type="button" class="btn btn-primary ml-auto" id="ann" data-toggle="modal" data-target="#Delete" style="    border-radius:90px 90px 90px 90px;padding:0px 8px;">
+                                                            <button title="Edit announcement" type="button" class="btn btn-primary ml-auto" id="ann" data-toggle="modal" data-target="#Edit" style="border-radius:90px 90px 90px 90px;padding:0px 8px;">
                                                                 <i class="icon ion-edit" style="font-size:14px;"></i></button></div></div>';
                                             echo    '</div>';
                                             echo '</div>';
@@ -121,6 +121,35 @@ $admin_fname = $this->session->userdata['login_success']['info']['admin_fname'];
                                     </div>
                                 </div>
                                 <!----END MODAL Delete-->
+                                <!----MODAL edit-->
+                                <div id="Edit" class="modal fade" role="dialog" tabindex="-1">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header" style="height: 58px;background-color: #bdedc1;">
+                                                <h4 class="modal-title" style="color: #11334f;">Edit announcement</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                                            
+                                            <form method="POST" name="delete_ann" action="" class="justify" style="width: 100%;margin: 0 auto;">
+                                            <div class="modal-body text-center">
+                                                <div class="form-group">
+                                                    <div class="form-row">
+                                                        <div class="col-xl-12"><label class="col-form-label" style="font-weight: normal;">Title</label></div>
+                                                        <div class="col-xl-12"><input name="etenant_lname" class="form-control" type="text" value="" required></div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="form-row">
+                                                        <div class="col-xl-12"><label class="col-form-label" style="font-weight: normal;">Content</label></div>
+                                                        <div class="col-xl-12"><textarea name="etenant_address" class="form-control" row="2" type="text"  required></textarea></div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                </div>
+                                                <div class="modal-footer"><button class="btn btn-primary" name="delete_ann" type="submit" style="background-color: #bdedc1;color: #11334f;border: none;">Yes</button></div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!----END MODAL edit-->
                                 </div>
                                 <p class="card-text" style="color: black;font-size: 18px;margin-bottom: 0px; text-align: right"><a style="color:#11334f"><?php echo $links; ?></a></p>
                             </div>
