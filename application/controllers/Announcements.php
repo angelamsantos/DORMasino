227deaefa489c31e99fbf506bbad79ee4f8d11fb
delacruz04 <2015081266@ust-ics.mygbiz.com>
2019-02-08 12:38:06 +0800

    Announcements display

--- application/controllers/Announcements.php
+++ application/controllers/Announcements.php
@@ -51,52 +51,132 @@ class Announcements extends CI_Controller{
             'admin_id' => $admin_id
         );
 
-        date_default_timezone_set("Asia/Manila");
-        $date = date("m-d-Y");
-        $path = "./assets/uploads/documents/".$date."/";
-      
-        $conf = array(
-            'upload_path' => "./assets/uploads/documents/".$date."/",
-            'allowed_types' => "xls|docx|doc|pdf",
-            'overwrite' => FALSE,
-            'max_size' => "20971520", // Can be set to particular file size , here it is 20 MB
-            'max_height' => "768",
-            'max_width' => "1024"
-        );
+        $file = $_FILES["file"]["name"];
+        $file_ext = pathinfo($file, PATHINFO_EXTENSION);
 
-        $this->load->library('upload', $conf);
+        $img = $_FILES["img"]["name"];
+        $img_ext = pathinfo($img, PATHINFO_EXTENSION);
 
-        if (!is_dir("assets/uploads/documents/".$date."/")) {
+        if (empty($file) & empty($img)) {
 
-            mkdir($path, 0777, true);
+            $this->announcements_model->publish($data);
+
+            $msg = '<div class="alert alert-success" style="font-size:15px;margin:0px"><center>Announcement posted!</center></div>';
+            $this->session->set_flashdata('msg', $msg);
+
+            redirect('Announcements/index');
+
+        } else {
+
+            if (!empty($file) && 
+                $file_ext == "xls" || $file_ext == "pdf" ||
+                $file_ext == "doc" || $file_ext == "docx") {
+
+                date_default_timezone_set("Asia/Manila");
+                $date = date("m-d-Y");
+                $path = "./assets/uploads/documents/".$date."/";
+        
+                $conf = array(
+                    'upload_path' => "./assets/uploads/documents/".$date."/",
+                    'allowed_types' => "xls|docx|doc|pdf",
+                    'overwrite' => FALSE,
+                    'max_size' => "20971520", // Can be set to particular file size , here it is 20 MB
+                    'max_height' => "768",
+                    'max_width' => "1024"
+                );
+
+                $this->load->library('upload', $conf);
+
+                if (!is_dir("assets/uploads/documents/".$date."/")) {
+
+                    mkdir($path, 0777, true);
+                    
+                }
+
+                    if (!$this->upload->do_upload('file')) {
+
+                        $msg = '<div class="alert alert-danger" style="font-size:15px;margin:0px"><center>Announcement not posted. File size is invalid!</center></div>';
+                        $this->session->set_flashdata('msg', $msg);
+
+                        redirect('Announcements/index');
+
+                    } else {
+
+                        $upload_data = $this->upload->data();
+
+                        $file_name = $upload_data['file_name'];
+
+                        $path .= $file_name;
+
+                        $this->announcements_model->publish($data);
+                        $this->announcements_model->publishFile($path);
+
+                        $msg = '<div class="alert alert-success" style="font-size:15px;margin:0px"><center>Announcement posted!</center></div>';
+                        $this->session->set_flashdata('msg', $msg);
+
+                        redirect('Announcements/index');
+
+                    }
+
+            } else if (!empty($img) && 
+
+                $img_ext == "png" || $img_ext == "gif" ||
+                $img_ext == "jpg" || $img_ext == "jpeg") {
+
+                    date_default_timezone_set("Asia/Manila");
+                    $date = date("m-d-Y");
+                    $path = "./assets/uploads/images/".$date."/";
             
-        }
-
-            if (!$this->upload->do_upload()) {
-
-                $msg = '<div class="alert alert-danger" style="font-size:15px;margin:0px"><center>Announcement not posted. File size or extension is invalid!</center></div>';
-                $this->session->set_flashdata('msg', $msg);
-
-                redirect('Announcements/index');
+                    $conf = array(
+                        'upload_path' => "./assets/uploads/images/".$date."/",
+                        'allowed_types' => "png|gif|jpg|jpeg",
+                        'overwrite' => FALSE,
+                        'max_size' => "20971520", // Can be set to particular file size , here it is 20 MB
+                    );
+    
+                    $this->load->library('upload', $conf);
+    
+                    if (!is_dir("assets/uploads/images/".$date."/")) {
+    
+                        mkdir($path, 0777, true);
+                        
+                    }
+    
+                        if (!$this->upload->do_upload('img')) {
+    
+                            $msg = '<div class="alert alert-danger" style="font-size:15px;margin:0px"><center>Announcement not posted. File size is invalid!</center></div>';
+                            $this->session->set_flashdata('msg', $msg);
+    
+                            redirect('Announcements/index');
+    
+                        } else {
+    
+                            $upload_data = $this->upload->data();
+    
+                            $file_name = $upload_data['file_name'];
+    
+                            $path .= $file_name;
+    
+                            $this->announcements_model->publish($data);
+                            $this->announcements_model->publishImg($path);
+    
+                            $msg = '<div class="alert alert-success" style="font-size:15px;margin:0px"><center>Announcement posted!</center></div>';
+                            $this->session->set_flashdata('msg', $msg);
+    
+                            redirect('Announcements/index');
+    
+                        }
 
             } else {
 
-                $upload_data = $this->upload->data();
-
-                $file_name = $upload_data['file_name'];
-
-                $path .= $file_name;
-
-                $this->announcements_model->publish($data);
-                $this->announcements_model->publishFile($path);
-
-                $msg = '<div class="alert alert-success" style="font-size:15px;margin:0px"><center>Announcement posted!</center></div>';
+                $msg = '<div class="alert alert-danger" style="font-size:15px;margin:0px"><center>Announcement not posted. File extension is invalid!</center></div>';
                 $this->session->set_flashdata('msg', $msg);
 
                 redirect('Announcements/index');
 
             }
         }
+    }
 
 }
 ?>
