018d5a321676bbc9791ab3cfbcb7d6c10e8a9038
delacruz04 <2015081266@ust-ics.mygbiz.com>
2019-03-08 10:45:07 +0800

    03/08/2019 10:45am

--- application/views/sent_view.php
+++ application/views/sent_view.php
@@ -112,7 +112,7 @@ $admin_fname = $this->session->userdata['login_success']['info']['admin_fname'];
                                                                         <label class="form-check-label">
                                                                             <input type="checkbox" class="chk_boxes1" name="archive_arr[]" value="'.$sent->send_id.'" style="margin-right:10px; margin-top:21px; margin-left: 20px; float:left;">
                                                                             <h6 class="d-flex" style="font-weight: bold;font-size:14px;margin-bottom: 2px;margin-top: 10px;">To: '.$sent->tenant_fname.' '.$sent->tenant_lname.'</h6>
-                                                                            <p class="d-flex" style="color: #868e96;font-size: 12px;margin-bottom: 8px;margin-left:15px;">'.$msg_date.'</p>
+                                                                            <p class="d-flex" style="color: #868e96;font-size: 12px;margin-bottom: 8px;margin-top:1px;margin-left:42px;">'.$msg_date.'</p>
                                                                         </label>
                                                                     </div>
                                                                     <div class="col-xl-9" >
